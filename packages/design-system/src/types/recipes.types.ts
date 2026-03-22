45046842054bae3c9976a218361ca8da2a15e42b
Justin <justin.blair.rankin@gmail.com>
2026-03-22 03:12:06 +0100

    fix: md linter fixes

--- packages/design-system/src/types/recipes.types.ts
+++ packages/design-system/src/types/recipes.types.ts
@@ -1,3 +1,1 @@
-import type { RecipeVariantProps } from '../../styled-system/css';
-
-export type RecipeProps<T extends (...args: any) => any> = NonNullable<RecipeVariantProps<T>>;
+export type RecipeProps<T extends (...args: any) => any> = NonNullable<Parameters<T>[0]>;
