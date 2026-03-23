d29d901b65e6e0b384cd9c293e515d9dc71f7dc0
Justin <justin.blair.rankin@gmail.com>
2026-03-23 18:18:03 +0100

    chore: adjust base color palette values

--- packages/design-system/src/palette/colors.base.ts
+++ packages/design-system/src/palette/colors.base.ts
@@ -11,9 +11,9 @@
 export const BASE_COLORS = {
   primary: 'oklch(48.8% 0.243 264.376)', // Tailwind blue-700
   secondary: 'oklch(49.6% 0.265 301.924)', // Tailwind purple-700
-  success: 'oklch(58.4% 0.194 149.214)', // Tailwind green-600
+  success: 'oklch(60.4% 0.194 149.214)', // Tailwind green-600
   warning: 'oklch(71% 0.188 70.08)', // Tailwind amber-500
-  danger: 'oklch(52% 0.245 27.325)', // Tailwind red-600
+  danger: 'oklch(55.7% 0.245 27.325)', // Tailwind red-600
   info: 'oklch(58.8% 0.158 241.966)', // Tailwind cyan-500
   default: 'oklch(65.3% 0.013 58.071)', // Tailwind stone-500
   grey: 'oklch(55.2% 0.016 285.938)', // Tailwind zinc-500
