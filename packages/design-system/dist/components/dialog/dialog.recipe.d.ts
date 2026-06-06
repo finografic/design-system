138aab52878c37113e958ce8af169e8b97618a8a
Justin <justin.blair.rankin@gmail.com>
2026-06-06 14:14:06 +1000

    feat: release v1.19.1

--- packages/design-system/dist/components/dialog/dialog.recipe.d.ts
+++ packages/design-system/dist/components/dialog/dialog.recipe.d.ts
@@ -2,7 +2,7 @@ import { SlotRecipeRuntimeFn } from "../../packages/design-system/styled-system/
 import { RecipeProps } from "../../recipes/recipes.types.js";
 
 //#region src/components/dialog/dialog.recipe.d.ts
-declare const dialogRecipe: SlotRecipeRuntimeFn<"header" | "footer" | "content" | "description" | "positioner" | "title" | "closeTrigger" | "root" | "backdrop" | "body", {
+declare const dialogRecipe: SlotRecipeRuntimeFn<"root" | "header" | "footer" | "content" | "description" | "backdrop" | "positioner" | "title" | "body" | "closeTrigger", {
   size: {
     xs: {
       content: {
--- packages/design-system/dist/components/menu/menu.recipe.d.ts
+++ packages/design-system/dist/components/menu/menu.recipe.d.ts
@@ -1,6 +1,6 @@
 import { SlotRecipeRuntimeFn, SlotRecipeVariantRecord } from "../../packages/design-system/styled-system/types/recipe.js";
 //#region src/components/menu/menu.recipe.d.ts
-declare const menuRecipe: SlotRecipeRuntimeFn<"content" | "positioner" | "arrow" | "arrowTip" | "root" | "separator" | "item" | "itemText" | "itemIndicator" | "itemGroup" | "itemGroupLabel" | "indicator", SlotRecipeVariantRecord<"content" | "positioner" | "arrow" | "arrowTip" | "root" | "separator" | "item" | "itemText" | "itemIndicator" | "itemGroup" | "itemGroupLabel" | "indicator">>;
+declare const menuRecipe: SlotRecipeRuntimeFn<"root" | "item" | "itemIndicator" | "content" | "positioner" | "separator" | "itemText" | "itemGroup" | "itemGroupLabel" | "arrow" | "arrowTip" | "indicator", SlotRecipeVariantRecord<"root" | "item" | "itemIndicator" | "content" | "positioner" | "separator" | "itemText" | "itemGroup" | "itemGroupLabel" | "arrow" | "arrowTip" | "indicator">>;
 //#endregion
 export { menuRecipe };
 //# sourceMappingURL=menu.recipe.d.ts.map
